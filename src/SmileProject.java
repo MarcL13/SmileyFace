d69029ac9c600b33e70909223a768f6df31e7092
MarcL13 <42978461+MarcL13@users.noreply.github.com>
2018-09-12 14:25:30 -0400

    Add files via upload

---
+++ src/SmileProject.java
@@ -0,0 +1,19 @@
+
+/**
+ * 
+ * @author Marc Lussier, Period 7
+ * Smile Project
+ *
+ */
+public class SmileProject {
+
+	public static void main(String[] args) {
+		System.out.println("\\\\\\\\\\\\");
+		System.out.println("| * * |");
+		System.out.println("|  *  |");
+		System.out.println("| \\_/ |");
+		System.out.print("\\_____/");
+
+	}
+
+}
